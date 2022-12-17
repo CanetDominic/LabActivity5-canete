61c9c68c4b1d5ca022cb1c1311146aebbd43bee4
DominicQT <dominiccanete75@gmail.com>
2022-12-17 17:50:47 +0800

    Add [package] main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,7 +2,8 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
+
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
@@ -13,5 +14,21 @@ public class LeapYearGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(480, 480);
         frame.setVisible(true);
+    }
+    public void solve(){
+        try {
+            int year = Integer.parseInt(tfYear.getText());
+            if (year % 400 == 0) {
+                JOptionPane.showMessageDialog(null, "Leap year");
+            } else if (year % 100 == 0) {
+                JOptionPane.showMessageDialog(null, "Not a leap year");
+            } else if (year % 4 == 0) {
+                JOptionPane.showMessageDialog(null, "Leap year");
+            } else {
+                JOptionPane.showMessageDialog(null, "Not a leap year");
+            }
+        } catch (Exception dominic){
+            JOptionPane.showMessageDialog(null,"Invalid Year Inputted!");
+        }
     }
 }
